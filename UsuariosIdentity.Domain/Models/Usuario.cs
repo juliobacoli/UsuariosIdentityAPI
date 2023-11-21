72729531be931548a8e2a1379d3827fa2fd619d4
Julio Bacoli <julio-microlins@hotmail.com>
2023-11-21 20:30:55 -0300

    Configurado o serviço de autorização do Identity na Program.cs

--- UsuariosIdentity.Domain/Models/Usuario.cs
+++ UsuariosIdentity.Domain/Models/Usuario.cs
@@ -4,5 +4,8 @@ namespace UsuariosIdentity.Domain.Models
 {
     public class Usuario : IdentityUser
     {
+        public Usuario() : base() { }
+
+        public DateTime DataNascimento { get; set; }
     }
 }
--- UsuariosIdentityAPI/Program.cs
+++ UsuariosIdentityAPI/Program.cs
@@ -1,15 +1,39 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Data.Common;
+using UsuariosIdentity.Domain.Models;
 using UsuariosIdentity.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<UsuarioDbContext>();
+var connectionString = builder.Configuration.GetConnectionString("UsuariosIdentityDb");
+builder.Services.AddDbContext<UsuarioDbContext>(o => o.UseSqlServer(connectionString));
+
+builder.Services
+    .AddIdentity<Usuario, IdentityRole>()
+    .AddEntityFrameworkStores<UsuarioDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "UsuariosIdentityAPI",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Julio Bacoli",
+            Email = "juliosantanabacoli@gmail.com",
+            Url = new Uri("https://www.linkedin.com/in/julio-bacoli/")
+        }
+    });
 
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
