dd3f33955628e7d616003e263d2d94f9c2982df6
Julio Bacoli <julio-microlins@hotmail.com>
2023-11-22 01:24:50 -0300

    Fluxo de cadastro de user validado e funcionando + Banco de dados OK

--- UsuariosIdentity.Infrastructure/Persistence/UsuarioDbContext.cs
+++ UsuariosIdentity.Infrastructure/Persistence/UsuarioDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using UsuariosIdentity.Domain.Models;
 
@@ -9,5 +10,30 @@ public class UsuarioDbContext : IdentityDbContext<Usuario>
     public UsuarioDbContext(DbContextOptions<UsuarioDbContext> opts) : base(opts)
     {
 
+    }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>()
+            .HasKey(x => x.Id);
+
+        modelBuilder.Entity<IdentityUserLogin<string>>()
+            .HasKey(p => new { p.LoginProvider, p.ProviderKey });
+
+        modelBuilder.Entity<IdentityUserRole<string>>()
+            .HasKey(r => new { r.UserId, r.RoleId });
+
+        modelBuilder.Entity<IdentityUserToken<string>>()
+            .HasKey(t => new { t.UserId, t.LoginProvider, t.Name });
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=JULIOAVELL-PC;Database=Seguro;Integrated Security=True;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
     }
 }
--- UsuariosIdentityAPI/Controllers/UsuariosController.cs
+++ UsuariosIdentityAPI/Controllers/UsuariosController.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using UsuariosIdentity.Application.Data.DTOs;
+using UsuariosIdentity.Domain.Models;
 
-namespace UsuariosIdentityAPI.Controllers
+namespace UsuariosIdentityAPI.Controllers;
+
+[ApiController]
+public class UsuariosController : Controller
 {
-    public class UsuariosController : Controller
+    private readonly IMapper _mapper;
+    private readonly UserManager<Usuario> _userManager;
+
+    public UsuariosController(IMapper mapper, UserManager<Usuario> userManager)
     {
-        [HttpPost]
-        public IActionResult CadastraUsuario(CreateUsuarioDTO dto)
-        {
-            throw new NotImplementedException();
-        }
+        _mapper = mapper;
+        _userManager = userManager;
+    }
+
+    [HttpPost]
+    [Route("api/cadastraUsuario")]
+    public async Task<IActionResult> CadastraUsuario(CreateUsuarioDTO dto)
+    {
+        Usuario usuario = _mapper.Map<Usuario>(dto);
+
+        var result = await _userManager.CreateAsync(usuario, dto.Password);
+
+        if (result.Succeeded)
+            return Ok("Usuário cadastrado.");
+        return BadRequest("Falha ao cadastrar usuário.");
+
     }
 }
